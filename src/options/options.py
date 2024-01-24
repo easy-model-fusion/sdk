6483700b25ee3c037fdaebd59bd629f77fcca738
qgerling <quentin.gerling@etu.unistra.fr>
2024-01-24 18:26:45 +0100

    creation options for text to image

--- src/options/options.py
+++ src/options/options.py
@@ -4,3 +4,7 @@ from abc import ABC
 class Options(ABC):
     prompt: str
     device: str
+
+    def __init__(self, prompt: str, device: str):
+        self.prompt = prompt
+        self.device = device
