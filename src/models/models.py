d9eeb590d2ec5e6987b408480ce35f456a4630af
qgerling <quentin.gerling@etu.unistra.fr>
2024-01-25 13:25:24 +0100

    Creation of all comment

--- src/models/models.py
+++ src/models/models.py
@@ -3,9 +3,16 @@ from src.options.options import Options
 
 
 class Models(ABC):
+    """
+    Abstract base class for all models
+    """
     model_name: str
 
     def __init__(self, model_name: str):
+        """
+        Initializes the model with the given name
+        :param model_name: The name of the model
+        """
         self.model_name = model_name
 
     @abstractmethod
