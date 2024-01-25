f3d7c9aa20a7d20f52f025f87176aa7e613ba8f1
Omar <chamaael@etu.unistra.fr>
2024-01-25 16:13:29 +0100

    main function test

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from src.models.models_management import ModelsManagement
+from src.models.models_text_generation import ModelsTextGeneration
 from src.models.models_text_to_image import ModelsTextToImage
+from src.options.options_text_generation import OptionsTextGeneration
 from src.options.options_text_to_image import OptionsTextToImage, Devices
 
-
 if __name__ == '__main__':
-    options = OptionsTextToImage(
-        prompt="Astronaut in a jungle, cold color palette, muted colors, detailed, 8k",
-        device=Devices.GPU,
-        image_width=256,
-        image_height=256
+    options = OptionsTextGeneration(
+        prompt="Astronaut in a Fur suit in an anime convention, disgust",
+        max_length=200,
+        temperature=0.2,
+        device=Devices.CPU
     )
 
-    model_stabilityai_name = "stabilityai/sdxl-turbo"
+    model_stabilityai_name = "stabilityai/stable-code-3b"
     model_management = ModelsManagement()
-    model_stabilityai = ModelsTextToImage(model_stabilityai_name)
+    model_stabilityai = ModelsTextGeneration(model_stabilityai_name)
 
     model_management.add_model(new_model=model_stabilityai, model_options=options)
     model_management.load_model(model_stabilityai_name)
 
-    image = model_management.generate_prompt()
-    image.show()
 
+    text = model_management.generate_prompt()
+    print(text)
