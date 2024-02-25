0468800133f2abbb112a18878e4c86f301f8b7ed
chamaael@etu.unistra.fr <chamaael@etu.unistra.fr>
2024-02-25 17:07:34 +0100

    Added options for tokenizers

---
+++ options/fast_pretrained_tokenizer_options.py
@@ -0,0 +1,85 @@
+import tokenizers
+import torch
+from transformers.tokenization_utils_base import VERY_LARGE_INTEGER
+
+from sdk.options import Options, Devices
+from typing import Optional, Union, Dict, Any, List
+from transformers import (PreTrainedModel, TFPreTrainedModel,
+                          PreTrainedTokenizer,
+                          PreTrainedTokenizerFast, ModelCard)
+
+from sdk.options.tokenizer_options import TokenizerOptions
+
+
+class FastPreTrainedTokenizerOptions(TokenizerOptions):
+    """
+    Options for Fast PreTrainedTokenizers
+    """
+
+    model_max_length : int = VERY_LARGE_INTEGER,
+    padding_side : Optional[str] = None,
+    truncation_side : Optional[str] = None,
+    chat_template : Optional[str] = None,
+    model_input_names : Optional[List[str]] = None,
+    bos_token : Optional[Union[str, tokenizers.AddedToken]] = None,
+    eos_token : Optional[Union[str, tokenizers.AddedToken]] = None,
+    unk_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+    sep_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+    pad_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+    cls_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+    mask_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+    additional_special_tokens: Optional[List[Union[str, tokenizers.AddedToken]]] = None,
+    clean_up_tokenization_spaces: bool = True,
+    split_special_tokens: bool = False,
+    tokenizer_object: tokenizers.Tokenizer = None ,
+    tokenizer_file: str = None,
+
+    def __init__(self,
+                 model_max_length: int = VERY_LARGE_INTEGER,
+                 padding_side: Optional[str] = None,
+                 truncation_side: Optional[str] = None,
+                 chat_template: Optional[str] = None,
+                 model_input_names: Optional[List[str]] = None,
+                 bos_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+                 eos_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+                 unk_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+                 sep_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+                 pad_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+                 cls_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+                 mask_token: Optional[Union[str, tokenizers.AddedToken]] = None,
+                 additional_special_tokens: Optional[List[Union[str, tokenizers.AddedToken]]] = None,
+                 clean_up_tokenization_spaces: bool = True,
+                 split_special_tokens: bool = False,
+                 tokenizer_object: tokenizers.Tokenizer = None,
+                 tokenizer_file: str = None,
+                 ):
+        # Initialize additional attributes
+        self.model_max_length = model_max_length
+        if padding_side :
+            self.padding_side = padding_side
+        if truncation_side:
+            self.truncation_side = truncation_side
+        if chat_template :
+            self.chat_template = chat_template
+        if model_input_names :
+            self.model_input_names = model_input_names
+        if bos_token :
+            self.bos_token = bos_token
+        if eos_token :
+            self.eos_token = eos_token
+        if unk_token :
+            self.unk_token = unk_token
+        if sep_token :
+            self.sep_token = sep_token
+        if pad_token :
+            self.pad_token = pad_token
+        if cls_token :
+            self.cls_token = cls_token
+        if mask_token :
+            self.mask_token = mask_token
+        if additional_special_tokens :
+            self.additional_special_tokens = additional_special_tokens
+        self.clean_up_tokenization_spaces = clean_up_tokenization_spaces
+        self.split_special_tokens = split_special_tokens
+        self.tokenizer_object = tokenizer_object
+        self.tokenizer_file = tokenizer_file
---
+++ options/tokenizer_options.py
@@ -0,0 +1,10 @@
+from abc import ABC
+
+
+
+
+class TokenizerOptions(ABC):
+    """
+    Abstract class defining the options used for tokenizers
+    """
+
