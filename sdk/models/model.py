f5d4c2496cff31374261dc05f4ec0da28846ae93
SkelNeXus <gerling.quentin@outlook.fr>
2024-03-20 15:42:57 +0100

    fix model.py

--- sdk/models/model.py
+++ sdk/models/model.py
@@ -1,6 +1,4 @@
 from abc import abstractmethod
-from sdk.options import Options
-from typing import Optional
 
 
 class Model:
@@ -28,6 +26,5 @@ class Model:
         raise NotImplementedError
 
     @abstractmethod
-    def generate_prompt(self, prompt: Optional[str],
-                        option: Options, **kwargs):
+    def generate_prompt(self, prompt: str, **kwargs):
         raise NotImplementedError
