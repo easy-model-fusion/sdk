0e27ff81a10f5ef3312c0884b3da8b65944afe11
qgerling <quentin.gerling@etu.unistra.fr>
2024-03-20 16:25:21 +0100

    demo for text generation

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import argparse
-from sdk.demo import DemoTextConv, DemoTextToImg
+from sdk.demo import DemoTextConv, DemoTextToImg, DemoTextGen
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Choose a Model type :'
-                                                 ' TextConv or TextToImg')
+                                                 ' TextConv, TextToImg '
+                                                 'or TextGen')
     subparser = parser.add_subparsers(dest='option')
 
     conv = subparser.add_parser('TextConv')
     img = subparser.add_parser('TextToImg')
+    gen = subparser.add_parser('TextGen')
     args = parser.parse_args()
 
     match args.option:
         case 'TextConv':
             DemoTextConv()
-        case ('TextToImg'):
+        case 'TextToImg':
             DemoTextToImg()
+        case 'TextGen':
+            DemoTextGen()
         case _:
             DemoTextToImg()
