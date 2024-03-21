3b1273b2f96f2adfab00db94abcf1276efb8c766
qgerling <quentin.gerling@etu.unistra.fr>
2024-03-21 16:32:13 +0100

    Modification of the text to image model

--- sdk/models/__init__.py
+++ sdk/models/__init__.py
@@ -2,5 +2,5 @@
 from sdk.models.model import Model
 from sdk.models.model_transformers import ModelTransformers
 from sdk.models.models_management import ModelsManagement
-from sdk.models.model_text_to_image import ModelTextToImage
+from sdk.models.model_diffusers import ModelDiffusers
 from sdk.models.model_text_conversation import ModelsTextConversation
