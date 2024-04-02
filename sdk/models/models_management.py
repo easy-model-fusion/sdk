0904f7f0dc2ea24ea69035bc078b8b0ffc9f6553
SkelNeXus <gerling.quentin@outlook.fr>
2024-04-02 22:56:43 +0200

    Manage models on CPU and GPU

--- sdk/models/models_management.py
+++ sdk/models/models_management.py
@@ -1,5 +1,7 @@
 from typing import Optional, Dict, Any
+
 from sdk.models import Model
+from sdk.options import Devices
 
 
 class ModelsManagement:
@@ -13,7 +15,8 @@ class ModelsManagement:
         """
         Initializes the ModelsManagement
         """
-        self.loaded_model: Optional[Model] = None
+        self.loaded_model_CPU: Optional[Model] = None
+        self.loaded_model_GPU: Optional[Model] = None
         self.loaded_models_cache: Dict[str, Model] = {}
 
     def add_model(self, new_model: Model) -> bool:
@@ -43,70 +46,146 @@ class ModelsManagement:
         Returns:
             bool: True if the model is successfully loaded
         """
-        if self.loaded_model:
-            print(
-                "Unload the currently loaded model before loading a new one.")
-            return False
-
         if model_name not in self.loaded_models_cache:
             print(f"Model '{model_name}' cannot be loaded: not found.")
             return False
 
-        self.loaded_model = self.loaded_models_cache[model_name]
-        if not self.loaded_model.load_model():
+        if (self.loaded_models_cache[model_name].device == Devices.CPU
+                or self.loaded_models_cache[model_name].device == Devices.CPU.value):
+            return self.load_model_on_cpu(model_name)
+
+        if (self.loaded_models_cache[model_name].device == Devices.GPU
+                or self.loaded_models_cache[model_name].device == Devices.GPU.value):
+            return self.load_model_on_gpu(model_name)
+
+    def load_model_on_cpu(self, model_name: str) -> bool:
+        """
+        Load a model with his name on the CPU.
+
+        Args:
+             model_name (str): The name of the model to load
+
+        Returns:
+            bool: True if the model is successfully loaded
+        """
+        if self.loaded_model_CPU:
+            print(
+                "Unload the currently loaded model before loading a new one.")
+            return False
+
+        self.loaded_model_CPU = self.loaded_models_cache[model_name]
+
+        if not self.loaded_model_CPU.load_model():
             print("Something went wrong while unloading the model.")
-            self.loaded_model = None
+            self.loaded_model_CPU = None
             return False
 
         return True
 
-    def unload_model(self) -> bool:
+    def load_model_on_gpu(self, model_name: str) -> bool:
+        """
+        Load a model with his name on the GPU.
+
+        Args:
+             model_name (str): The name of the model to load
+
+        Returns:
+            bool: True if the model is successfully loaded
+        """
+        if self.loaded_model_GPU:
+            print(
+                "Unload the currently loaded model before loading a new one.")
+            return False
+
+        self.loaded_model_GPU = self.loaded_models_cache[model_name]
+
+        if not self.loaded_model_GPU.load_model():
+            print("Something went wrong while unloading the model.")
+            self.loaded_model_GPU = None
+            return False
+
+        return True
+
+    def unload_model(self, model_name: str) -> bool:
         """
         Unload the loaded model
+
+        Args:
+             model_name (str): The name of the model to load
 
         Returns:
             bool: True if the model is successfully unloaded
         """
-        if not self.loaded_model:
-            print("No model loaded to unload.")
-            return False
+        if (self.loaded_models_cache[model_name].device == Devices.CPU
+                or self.loaded_models_cache[model_name].device == Devices.CPU.value):
 
-        if not self.loaded_model.unload_model():
-            print("Something went wrong while unloading the model.")
-            return False
-        self.loaded_model = None
+            if not self.loaded_model_CPU:
+                print("No model loaded to unload.")
+                return False
+
+            if not self.loaded_model_CPU.unload_model():
+                print("Something went wrong while unloading the model.")
+                return False
+            self.loaded_model_CPU = None
+
+        if (self.loaded_models_cache[model_name].device == Devices.GPU
+                or self.loaded_models_cache[model_name].device == Devices.GPU.value):
+
+            if not self.loaded_model_GPU:
+                print("No model loaded to unload.")
+                return False
+
+            if not self.loaded_model_GPU.unload_model():
+                print("Something went wrong while unloading the model.")
+                return False
+            self.loaded_model_GPU = None
+
         return True
 
     def generate_prompt(self, prompt: Any,
-                        model_name: Optional[str] = None, **kwargs):
+                        model_name: str, **kwargs):
         """
         Generates the prompt for the loaded model with his stored options
 
         Args:
             prompt (Any): The prompt to generate
-            model_name: (Optional[str]): the model name to load
+            model_name: (str): the model name to load
             kwargs: more parameters to pass to the prompt generator
 
         Returns:
             The object of type link with the model category
         """
-        if model_name:
-            if self.loaded_model.model_name != model_name:
-                self.unload_model()
+        if (self.loaded_models_cache[model_name].device == Devices.CPU
+                or self.loaded_models_cache[model_name].device == Devices.CPU.value):
 
-        if not self.loaded_model:
-            if model_name:
-                print("No model loaded to generate.")
-                return
+            if self.loaded_model_CPU.model_name != model_name:
+                self.unload_model(model_name)
 
-            self.load_model(model_name=model_name)
+            if not self.loaded_model_CPU:
+                self.load_model(model_name=model_name)
 
-        return (
-            self.loaded_model.generate_prompt(
-                prompt,
-                **kwargs
+            return (
+                self.loaded_model_CPU.generate_prompt(
+                    prompt,
+                    **kwargs
+                )
             )
-        )
+
+        if (self.loaded_models_cache[model_name].device == Devices.GPU
+                or self.loaded_models_cache[model_name].device == Devices.GPU.value):
+
+            if self.loaded_model_GPU.model_name != model_name:
+                self.unload_model(model_name)
+
+            if not self.loaded_model_GPU:
+                self.load_model(model_name=model_name)
+
+            return (
+                self.loaded_model_GPU.generate_prompt(
+                    prompt,
+                    **kwargs
+                )
+            )
 
     def print_models(self):
         """
@@ -114,6 +193,8 @@ class ModelsManagement:
         """
         print("Models in cache:")
         for model_name, model_instance in self.loaded_models_cache.items():
-            selected_indicator = (
-                "(selected)" if model_instance == self.loaded_model else "")
-            print(f"- {model_name} {selected_indicator}")
+            selected_indicator_CPU = (
+                "(CPU selected)" if model_instance == self.loaded_model_CPU else "")
+            selected_indicator_GPU = (
+                "(GPU selected)" if model_instance == self.loaded_model_GPU else "")
+            print(f"- {model_name} {selected_indicator_CPU} {selected_indicator_GPU}")
